c4f6fed2041951f7f8a858ac4fb1c3eec120e379
John Mendez <johnmendezgarcia@gmail.com>
2020-07-21 15:29:27 -0500

    commit proyecto springBoot dia 4

---
+++ src/main/java/com/springboot/LearnReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnReactiveApplication.class, args);
+	}
+
+}
